bedfcffc9799a8b0b1f12c1ad159d12eff0e4451
nobya07 <gajendrapunekar017@gmail.com>
2026-02-06 22:52:10 +0530

    separate-ec2-1

--- separate-ec2/backend/app.py
+++ separate-ec2/backend/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/api/health")
 def health():
-    return "Backend is running"
+    return "Status: Flask backend running locally"
+
+if __name__ == "__main__":
+    app.run(port=5000,host="0.0.0.0", debug=True )
