5596a5478ad726d1c5c28ad25e8f73e178d5bf1f
nobya07 <gajendrapunekar017@gmail.com>
2026-02-07 19:33:42 +0530

    3

--- separate-ec2/frotend/index.js
+++ separate-ec2/frotend/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const fetch = require("node-fetch");
+
+console.log("Starting frontend...");
 
 const app = express();
 
 app.get("/status", async (req, res) => {
   try {
-    const response = await fetch(
-      "http://13.204.67.103:5000/api/health"
-    );
+    const response = await fetch("http://13.204.67.103:5000/api/health");
     const text = await response.text();
     res.send(text);
-  } catch {
-    res.send("Backend not reachable");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Backend not reachable");
   }
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Frontend running on port 3000");
 });
